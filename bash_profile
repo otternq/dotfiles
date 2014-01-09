8b987f0bc8080ef170e8c1a1c8178ee9f9d3eeb7
Nick Otter <notter@ishoutout.com>
2014-01-09 01:19:44 -0800

    adding initial dotfiles and bootstrap

---
+++ bash_profile
@@ -0,0 +1,2 @@
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/Development/adt-bundle-mac-x86_64-20131030/sdk/tools
---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -s ${PWD}/bash_profile ~/.bash_profile
+ln -s ${PWD}/vimrc ~/.vimrc
+
+echo i
+
+if ! [ -L "$HOME/bin" ]; then
+    ln -s ${PWD}/bin ~/bin
+else
+    echo "$HOME/bin a;ready linked"
+fi
+
+chmod a+x ~/bin/*
